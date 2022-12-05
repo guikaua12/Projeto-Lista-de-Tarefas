c7c8e2bbc0fdeee01b44046e25255086df65d19a
Approximations <Approximations@APPROXIMATIONS>
2022-12-05 14:36:42 -0300

    Adicionado localStorage como armazenamento

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-let Main = {
+let tasks = {
+    'porra': {
+        done: false,
+        task: 'porraa'
+    },
+    'porra2': {
+        done: false,
+        task: 'porraa2'
+    }
+}
+
+const Main = {
     init: function() {
         this.cacheSelectors();
         this.bindEvents();
+        this.loadTasks();
     },
 
     cacheSelectors: function() {
@@ -11,11 +23,44 @@ let Main = {
         this.$list = document.querySelector('#list');
     },
 
+    addOrUpdateTask: function(task) {
+        this.tasks[task.id] = task;
+        this.saveTasks();
+    },
+
+    removeTask: function(id) {
+        delete this.tasks[id];
+        this.saveTasks();
+    },
+
+    loadTasks: function() {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        this.tasks = !tasks && {} || tasks;
+
+        for (let taskKey in this.tasks) {
+            const currentTask = this.tasks[taskKey];
+            this.$list.innerHTML += `
+            <li id="${currentTask.id}" class="${currentTask.done && 'done'}">
+                <div class="check"></div>
+                <label class="task">${this.tasks[taskKey].task}</label>
+                <button class="remove">x</button>
+            </li>
+            `;
+        }
+        this.cacheSelectors();
+        this.bindEvents();
+    },
+
+    saveTasks: function() {
+        tasks = JSON.stringify(this.tasks);
+        localStorage.setItem('tasks', tasks);
+    },
+
     bindEvents: function() {
         const self = this;
 
         this.$checkButtons.forEach(button => {
-            button.onclick = self.Events.checkButton_click;
+            button.onclick = self.Events.checkButton_click.bind(this);
         });
 
         this.$removeButtons.forEach(button => {
@@ -25,22 +70,39 @@ let Main = {
         this.$inputTask.onkeydown = self.Events.inputTask_keydown.bind(this);
     },
 
+    randomId: function() {
+        let id = '';
+        const chars = 'abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSUVWXYZ1234567890';
+        for (let i = 0; i < chars.length; i++) {
+            const random = Math.round(Math.random() * i);
+            id += chars[random];
+        }
+        return id;
+    },
+
     Events: {
         checkButton_click: function(e) {
             const parent = e.target.parentElement;
             const isDone = parent.classList.contains('done');
+            let currentTask = this.tasks[parent.id];
 
+            console.log(currentTask)
             if(!isDone) {
                 parent.classList.add('done');
+                currentTask.done = true;
+                this.addOrUpdateTask(currentTask);
                 return;
             }
 
             parent.classList.remove('done');
+            currentTask.done = false;
+            this.addOrUpdateTask(currentTask);
         },
 
         removeButton_click: function(e) {
             const parent = e.target.parentElement;
-            parent.classList.add('removed');
+            this.removeTask(parent.id);
+            parent.remove();
         },
 
         inputTask_keydown: function(e) {
@@ -48,13 +110,22 @@ let Main = {
             const task = e.target.value;
             if(task.trim() === '') return;
 
+            const taskObj = {
+                id: this.randomId(),
+                done: false,
+                task: task
+            };
+
+            this.addOrUpdateTask(taskObj);
+
             this.$list.innerHTML += `
-            <li>
+            <li id="${taskObj.id}">
                 <div class="check"></div>
                 <label class="task">${task}</label>
                 <button class="remove">x</button>
             </li>
-            `
+            `;
+
             e.target.value = '';
 
             this.cacheSelectors();
